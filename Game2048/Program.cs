32ad2465ce9886e2e482915d2788f6c87fcacc01
M <mihail.petuxov@yandex.ru>
2020-04-25 13:18:09 +0500

    Еще набросок
    Забыл сохранить

--- Game2048/Program.cs
+++ Game2048/Program.cs
@@ -6,7 +6,43 @@ namespace Game2048_console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program p = new Program();
+            p.Start(4);
+        }
+
+        Game game;
+        void Start(int size)
+        {
+            game = new Game(size);
+            game.Start();
+            while (true)
+            {
+                Show(game);
+                switch(Console.ReadKey(false).Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        game.MoveUp();
+                        break;
+                    case ConsoleKey.DownArrow:
+                        game.MoveDown();
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        game.MoveLeft();
+                        break;
+                    case ConsoleKey.RightArrow:
+                        game.MoveRight();
+                        break;
+
+                }
+        }
+
+        static void Show(Game game)
+        {
+                /* for y
+                 *      for x
+                 *      setcursor
+                 *      write Value
+                 *      */
         }
     }
 }
